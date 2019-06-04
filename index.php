bd0fbf637bedfe818e945107ae7960ce5a80bb8e
kedir <kedirrobert@gmail.com>
2019-06-04 14:16:58 -0500

    primeros cambios
    mi primera pagina web para heroku

--- index.php
+++ index.php
@@ -1,1 +1,374 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+      <script src="js/jquery-2.1.1.min.js"></script>
+      <script src="js/funciones.js"></script>
+      <link rel="stylesheet" href="css/index.css">
+  </head>
+  <body>
+    <!-- CABECERA DE PAGINA -->
+    <header>
 
+      <nav>
+        <div class="nav-wrapper">
+          <a href="#!" class="brand-logo white-text">KedirGC</a>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+          <ul class="right hide-on-med-and-down ">
+            <li><a class="white-text" href="#inicio">inicio</a></li>
+            <li><a class="white-text" href="#nosotros">nosotros</a></li>
+            <li><a class="white-text" href="#valores">valores</a></li>
+            <li><a class="white-text" href="#contactenos">contactenos</a></li>
+          </ul>
+        </div>
+      </nav>
+
+      <ul class="sidenav" id="mobile-demo">
+        <li><a href="#sass">inicio</a></li>
+        <li><a href="#nosotros">nosotros</a></li>
+        <li><a href="#valores">valores</a></li>
+        <li><a href="#contactenos">contactenos</a></li>
+      </ul>
+
+    </header>
+
+<!-- SLIDER SLIDER SLIDER SLIDERSLIDER -->
+  <section>
+
+<div class="fullscreen">
+
+
+        <div class="slider">
+    <ul class="slides">
+      <li>
+        <img src="1.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>Este es Mi Dog!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="2.png"> <!-- random image -->
+        <div class="caption left-align">
+          <h3>Bienvenido a mi Ciudad</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="3.jpg"> <!-- random image -->
+        <div class="caption right-align">
+          <h3>Bienvenido a Nueva Zelanda</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+      <li>
+        <img src="5.jpg"> <!-- random image -->
+        <div class="caption center-align">
+          <h3>Bienvenido a mi bosque!</h3>
+          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+        </div>
+      </li>
+    </ul>
+  </div>
+</div>
+  </section>
+<!-- ***********************Bienvenido******************************************* -->
+
+<div class="row" id="inicio">
+
+  <div class="col s12 " >
+
+  <div class="col s6 #c5e1a5 light-green lighten-3">
+    <div class="center" >
+      <h3>BIENVENIDO</h3>
+      <p>Pasteleria Ancky te invita a disfrutar de las   más deliciosas tortas,
+         postres y bocaditos preparados con las mejores recetas caseras
+          y dos ingredientes de la casa: cariño y dedicación.
+          En Ancky no usamos ningún tipo de preservante; es por ello que todo lo que preparamos,
+           tiene un sabor delicioso y fresco como si fuera  “hecho en casa”.</p>
+          </div></div>
+
+<div class="col s6  #c5e1a5 light-green lighten-3">
+<div class="center" ><p><br><br><br>
+  <img src="catalogo.jpg" class="responsive-img"  alt="" width="100%" height="100%"></p><br><br>
+</div>
+</div>
+</div>
+
+    </div>
+
+
+<!--**********************************************************************> -->
+
+
+ <div class="container">
+
+  <div class="row" >
+      <div class="col s12 m4">
+        <div class="card-panel teal">
+        <span class="white-text">
+            <div class="center" >
+          <img  class="responsive-img" src="becky.jpg" class="circle responsive-img" alt=""  >
+          <h4>Dana Garcia</h4>
+              </div>
+        </span>
+          <p class="center-align white-text">Alumno de la carrera de Computaciòn e informatica en el instituto cesca</p>
+        </div>
+      </div>
+
+      <div class="col s12 m4">
+        <div class="col s12 ">
+          <div class="card-panel blue">
+                <span class="white-text">
+              <div class="center-align" >
+                  <img  class="responsive-img" src="becky.jpg" class="circle responsive-img" alt=""  >
+                    <h4>Dana Garcia</h4>
+                    </span>
+          <!-- <p class="center"> -->
+          <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+            I am convenient because</div>
+          <!-- </p> -->
+          </span>
+          </div>
+      </div>
+    </div>
+
+    <div class="col s12 m4">
+      <div class="col s12 ">
+        <div class="card-panel #5d4037 brown darken-2">
+            <div class="center-align" >
+                  <img  class="responsive-img" src="becky.jpg" class="circle responsive-img" alt=""  >
+                    <h4 class="white-text">Dana Garcia</h4>
+
+
+        <p class="center"></p>
+        <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+          I am convenient because</div>
+        </span>
+        </div>
+    </div>
+  </div>
+
+
+  </div>   <!-- Note that "m4 l3" was added -->
+
+  </div>
+
+
+<!-- *****************************************Nosotros********************************************************************* -->
+
+<div class="row" id="nosotros">
+     <div class="col s12 #b2dfdb teal lighten-4">
+        <div class="center">
+
+       <div class="row">
+           <div class="col s12 ">
+             <div class="card blue-grey darken-1">
+               <div class="card-content white-text">
+                 <span class="card-title">Nosotros</span>
+                 <p>Pastelería Ancky empezó en el 2003 en el distrito de Surquillo como una aventura familiar, impulsados por la iniciativa de nuestra madre. Comenzamos preparando todo tipo de tortas, pasteles y bocaditos con nuestras mejores recetas caseras. Poco a poco gracias a la publicidad boca a boca de nuestros amigos y allegados, comenzaron a llegar nuestros primeros clientes, los cuales nos siguen acompañando. Así nos hicimos conocidos y empezamos a tener una increíble y fiel clientela a quien le agradecemos de todo corazón.
+Los que nos motivan a seguir brindando lo mejor de nosotros, son Uds., nuestros clientes. Queremos que cada celebración o antojo sea algo especial. En Ancky estamos orgullosos de ser parte de ti, y nos hagan partícipes de sus momentos
+especiales.
+Nuestra intención siempre es ofrecerle lo mejor, con la calidad y frescura de siempre, así como nuestras tortas, que aseguran ser la mejor elección para quedar bien ante los amigos, compañeros y familiares.
+Si tienes algún diseño o creación en mente, para alguna celebración especial, estamos a tu entera disposición, con la misma atención personalizada y cálida de siempre en nuestros 5 locales, contamos con 2 en Surquillo, 1 en San Luis, 1 en Surco y 1 en Chorrillos.</p>
+               </div>
+
+             </div>
+           </div>
+         </div>
+
+         </div>
+       </div>
+
+       </div>
+
+
+<!-- ******************************************************************************************** -->
+<div class="parallax-container">
+     <div class="parallax"><img src="6.jpeg"></div>
+   </div>
+<!-- ************************************************************************************************ -->
+  <div class="container">
+    <div class="section" id="valores">
+
+      <div class="row">
+        <div class="col s12 center">
+          <h3><i class="mdi-content-send brown-text"></i></h3>
+          <h4>Valores</h4>
+          <p class="left-align light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+<!-- ****************************************************************************************** -->
+<div class="container">
+  <div class="section">
+
+    <!--   Icon Section   -->
+    <div class="row">
+      <div class="col s12 m3">
+        <div class="icon-block">
+          <h2 class="center brown-text"><i class="material-icons">grade</i></h2>
+          <h5 class="center">CALIDAD</h5>
+
+          <p class="light">  Hacemos el trabajo en equipo y bien hecho desde
+el principio, damos lo mejor de nosotros y
+buscamos siempre soluciones simples y efectivas .</p>
+        </div>
+      </div>
+
+      <div class="col s12 m3">
+        <div class="icon-block">
+          <h2 class="center brown-text"><i class="material-icons">lightbulb_outline</i></h2>
+          <h5 class="center">INNOVACIÓN</h5>
+
+          <p class="light">Nuestro trabajo es un trabajo vivo, en continuo cambio. Con una gran base de tradición
+             pero con una importante capacidad de adaptación a los nuevos tiempos.</p>
+        </div>
+      </div>
+      <!-- ************************************* -->
+      <div class="col s12 m3">
+        <div class="icon-block">
+          <h2 class="center brown-text"><i class="material-icons">offline_pin</i></h2>
+          <h5 class="center">RESPONSABILIDAD</h5>
+
+          <p class="light">  Obramos con perseverancia para lograr
+            nuestros compromisos, teniendo presente la
+            protección de los recursos naturales y el
+            medio ambiente.</p>
+        </div>
+      </div>
+
+      <!-- *********************************** -->
+      <div class="col s12 m3">
+        <div class="icon-block">
+          <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
+          <h5 class="center">SERVICIO</h5>
+
+          <p class="light"> Servimos con devoción y orientamos todo nuestro
+esfuerzo para asegurar la lealtad de
+ciertos consumidores.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<div class="section container">
+<div class="row" id="contacto">
+  <form class="col s12" >
+      <div class="row card-panel">
+        <h3 class="center-align">CONTACTO</h3>
+          <article class="col s6 offset-s3">
+            <form method="POST" action="formulario-contacto.php">
+              <div class="input-field">
+                <i class="material-icons prefix">perm_identity</i>
+                <label for="nombre">Nombre</label>
+                <input type="text" name="nombre" required>
+              </div>
+
+              <div class="input-field">
+                <i class="material-icons prefix">person_pin</i>
+                <label for="apellido">Apellido</label>
+                <input type="text" name="apellido" required>
+              </div>
+
+              <div class="input-field">
+                <i class="material-icons prefix">email</i>
+                <label for="email">Email</label>
+                <input type="email" name="email" required>
+              </div>
+
+              <div class="input-field">
+                <i class="material-icons prefix">mode_edit</i>
+                <label for="mensaje">Mensaje</label>
+                <textarea name="mensaje" id="" rows="10" class="materialize-textarea"  length="140" required></textarea>
+              </div>
+
+              <p class="center-align">
+                <button class="waves-effect waves-light btn" type="submit"><i class="material-icons right">send</i>enviar</button>
+              </p>
+
+            </form>
+
+          </article>
+      </div>
+
+
+  </form>
+
+</div>
+
+	</div>
+<!-- ***************************************************** -->
+
+
+
+              <!-- <div class="container"> -->
+                <!-- Page Content goes here -->
+
+      <footer class="page-footer teal">
+                <div class="row">
+                  <div class="col s12 " >
+
+                        <div class="col s6 teal">
+                          <div class="center" >
+                              <img src="locales.png" class="responsive-img"  alt="" width="100%" height="100%">
+                                </div></div>
+
+                      <div class="col s6  teal">
+                      <div class="center" >
+                        <img src="novedades.png" class="responsive-img"  alt="" width="100%" height="100%">
+                      </div></div>
+                  </div>
+                    </div>
+
+            <!-- </div> -->
+
+
+            </body>
+
+
+
+        <div class="footer-copyright">
+          <div class="container">
+          © 2019 Kedir Garcia Calderon Todos los Derechos Reservados | Diseño: El Zorro <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+          </div>
+        </div>
+      </footer>
+
+
+
+
+
+
+
+
+
+ <script src="js/init.js"> </script>
+ <script src="js/materialize.min.js"></script>
+ <script>
+
+
+ $(document).ready(function(){
+   $('.sidenav').sidenav();
+
+  });
+  $(document).ready(function(){
+   $('.slider').slider();
+ });
+ $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+ </script>
+  </body>
+</html>
--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,15 @@
-
+function validar_login()
+{
+    var dni = document.getElementById("dni").value;
+    var contraseña = document.getElementById("contraseña").value;
+    if (dni=="")
+    {
+        alert("debe de ingresar dni");
+        return false;
+    }
+    if(contraseña=="")
+    {
+        alert("debe de ingresar contraseña");
+        return false;
+    }
+}
